7f777eaebf40ba59ba671a3c446a74d1545c3938
z3ut <z3ut@protonmail.com>
2019-07-31 09:37:37 +0300

    Add game size selector

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,16 @@ const cellSize = '1em';
 
 const gridElement = document.querySelector<HTMLElement>('[data-grid]');
 const statusElement = document.querySelector<HTMLElement>('[data-status]');
+const gameSizeElement = document.querySelector<HTMLSelectElement>('[data-game-size]');
 const newGameBtnElement = document.querySelector<HTMLElement>('[data-new-game-btn]');
 
 let game = new Game(gridElement, statusElement, cellSize);
-game.createNewGame(fieldWidth, fieldHeight, 10);
+game.createNewGame(getSelectedGameSize(), getSelectedGameSize(), 10);
 
 newGameBtnElement.addEventListener('click', () => {
-  game.createNewGame(fieldWidth, fieldHeight, 10);
+  game.createNewGame(getSelectedGameSize(), getSelectedGameSize(), 10);
 });
+
+function getSelectedGameSize() {
+  return +gameSizeElement.value || 10;
+}
